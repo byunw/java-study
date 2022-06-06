6b77be739124f46f1e20677371643483e41a1220
byunw <wbyun1461@gmail.com>
2022-06-06 13:28:16 +0900

    Create javaArray.java

---
+++ javaArray.java
@@ -0,0 +1,23 @@
+public class javaArray{
+   
+   public static void main(String args[]){
+          
+          //참조변수만 생성      
+          int array[];
+          
+          //어레이 생성 
+          array=new int[5];
+     
+          //어레이에 값 저장
+          array[0]=1;
+          array[1]=2;
+          array[2]=3;
+          array[3]=4;
+          array[4]=5;
+          
+          //어레이 값으로 7 출력하기
+          System.out.println(array[4]+array[1]);
+          
+   }
+  
+}
