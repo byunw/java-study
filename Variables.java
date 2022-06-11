8836bb238f3e8162a4da31ea7e725ffd4f56ec8a
byunw <wbyun1461@gmail.com>
2022-06-11 14:23:03 +0900

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,23 @@
+public class Variables{
+  public static void main(String args[]){
+      //객체 생성
+      time t1=new time();
+      t1.showHour();
+  }
+}
+
+class time{
+  
+  //인스턴스 변수
+  int hour;
+  int minute;
+  int second;
+  
+  //메서드 정의
+  void showHour(){
+    int lv=0;//인스턴스 변수
+    System.out.println(lv);
+  }
+  
+  
+}
