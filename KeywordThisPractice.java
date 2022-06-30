16aca6fad2188cd5e9e23b930b783a5732aed24e
byunw <wbyun1461@gmail.com>
2022-06-30 17:49:57 +0900

    Create KeywordThisPractice.java

---
+++ KeywordThisPractice.java
@@ -0,0 +1,29 @@
+class Car{
+      
+      //instance variables 
+      String color;
+      String gearType;
+      int door;
+      
+      //default constructor
+      Car(){
+         //calling nondefault constructor
+         this("white","auto",4);
+      }
+      
+      //nondefault constructor
+      Car(String c,String g,int d){
+         color=c;
+         gearType=g;
+         door=d;
+      }  
+}
+
+public class KeywordThisPractice{
+       public static void main(String[] args){
+               Car c=new Car();
+               System.out.println(c.color);
+               System.out.println(c.gearType);
+               System.out.println(c.door);
+       }
+}
