0189ad13fbd8468bd249393044ddbacf43c6d9a9
byunw <wbyun1461@gmail.com>
2022-08-07 12:42:49 +0900

    Create Operator.java

---
+++ Operator.java
@@ -0,0 +1,14 @@
+public class Operator{
+  public static void main(String[] args){
+     
+    int x=5;
+    int y=x*2=1;
+    
+    //prints out 11
+    System.out.println(y);
+    
+    //prints out 11
+    System.out.println(x*2+1);
+    
+  } 
+}
