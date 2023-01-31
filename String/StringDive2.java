a2b4af217df50dcde7632f8da14485ee046086ed
byunw <wbyun1461@gmail.com>
2023-01-31 20:59:06 +0900

    Create StringDive2.java

---
+++ String/StringDive2.java
@@ -0,0 +1,17 @@
+public class StringDive2{
+       
+        public static void main(String[] args){
+               
+               String s1 = new String("abc");
+               String s2 = new String("abc");
+              
+               if(s1!=s2){
+                  System.out.println("s1과 s2는 다른 메모리지역을 가리킴!");
+               }
+          
+        }
+        
+}
+                 
+            
+               
